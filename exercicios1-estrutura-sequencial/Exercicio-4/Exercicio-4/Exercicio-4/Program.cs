de6b5ab0326211d55eaf246002fccf9c83ce537e
João Vitor Moreira da Silva <joaovitor2710@gmail.com>
2023-06-07 19:59:40 -0300

    att 0,4

--- exercicios1-estrutura-sequencial/Exercicio-4/Exercicio-4/Exercicio-4/Program.cs
+++ exercicios1-estrutura-sequencial/Exercicio-4/Exercicio-4/Exercicio-4/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Exercicio_4
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numero, horas;
+            double valorHora, salario;
+
+            numero = int.Parse(Console.ReadLine());
+            horas = int.Parse(Console.ReadLine());
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = valorHora * horas;
+
+            Console.WriteLine("NUMERO = " + numero);
+            Console.WriteLine("Salario = R$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
