f6d2116c6e860d356033b06afabe731c78e18178
João Vitor Moreira da Silva <joaovitor2710@gmail.com>
2023-06-08 17:07:38 -0300

    att

---
+++ exercicios3-estrutura-while/Exercicio-1/Exercicio-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Senha = 1000;
+
+            int Tentativas = int.Parse(Console.ReadLine());
+
+            while(Senha != Tentativas)
+            {
+                Console.WriteLine("Senha Invalida");
+                Tentativas = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
