cef01569bb0c4fbd880369eccc9cfb922bedeaf7
João Vitor Moreira da Silva <joaovitor2710@gmail.com>
2023-06-25 18:10:23 -0300

    Update

---
+++ Seção-4 Classes,métodos e membros estáticos/Exercicio-1/Exercicio-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Exercicio_1;
+using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Pessoa pessoa1 = new Pessoa();
+           Pessoa pessoa2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa");
+            Console.WriteLine("Nome: ");
+            pessoa1.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            pessoa1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.WriteLine("Nome: ");
+            pessoa2.Nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+
+            if(pessoa1.Idade > pessoa2.Idade)
+                Console.WriteLine("Pessoa mais velha: " + pessoa1.Nome);
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + pessoa2.Nome);
+            }
+            
+
+            
+
+        }
+    }
+}
