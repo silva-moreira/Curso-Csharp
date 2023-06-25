67cc5a50b74c0a4df9b07f6b3fe3bf4b8c16b1fa
João Vitor Moreira da Silva <joaovitor2710@gmail.com>
2023-06-25 20:37:31 -0300

    Update

--- Seção-4 Classes,métodos e membros estáticos/Exercicio-2/Exercicio-2/Program.cs
+++ Seção-4 Classes,métodos e membros estáticos/Exercicio-2/Exercicio-2/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercicio_2;
+using System;
+using System.Globalization;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario();
+            Funcionario funcionario2 = new Funcionario();
+
+            Console.WriteLine("Entre com os dados do primeiro Funcionario");
+            Console.WriteLine("Nome: ");
+            funcionario1.Nome = Console.ReadLine();
+            Console.WriteLine("Salario: ");
+            funcionario1.Salario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com os dados do segundo funcionario");
+            Console.WriteLine("Nome: ");
+            funcionario2.Nome = Console.ReadLine();
+            Console.WriteLine("Salario: ");
+            funcionario2.Salario = int.Parse(Console.ReadLine());
+
+            double Total = (funcionario1.Salario + funcionario2.Salario) / 2;
+
+            Console.WriteLine("Salario medio: " + Total.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
